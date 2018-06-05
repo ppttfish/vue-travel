0e5e7e70aac2a8d9981a59f7c18eaa912bce08bb
ppttfish <ppttfish@163.com>
2018-06-05 19:58:12 +0800

    swiper finished

---
+++ vue-travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in list" :key="item.id">
+        <img  class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      list: [
+        {
+          id: '0001',
+          imgUrl: '/static/img/swiper1.jpeg'
+        },
+        {
+          id: '0002',
+          imgUrl: '/static/img/swiper2.jpeg'
+        },
+        {
+          id: '0003',
+          imgUrl: '/static/img/swiper3.jpeg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+    opacity: .7 !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 56.26%
+    .swiper-img
+      width: 100%
+</style>
